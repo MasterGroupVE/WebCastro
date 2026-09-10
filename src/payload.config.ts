25efd6070ddf6875a9e8095ff51cfeb01b416c14
vmontoyaLMG <victor.montoya8@gmail.com>
2026-09-09 21:23:14 -0400

    admin activo storage en r2

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -59,6 +59,7 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
+      max: 20,
     },
   }),
   collections: [Pages, Posts, Media, Categories, Users],
